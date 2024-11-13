7328c2d2db44f8b6aa293f21501eaf10c7da136e
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-13 14:57:04 +0100

    feat: javascript logic added

--- js/script.js
+++ js/script.js
@@ -38,3 +38,42 @@ const teamMembers = [
 ];
 
 const teamContainerElem = document.querySelector(".team-container");
+
+////////////////
+// FUNCTIONS //
+//////////////
+const createCard = (member) => {
+  const {name, role, email, img} = member;
+  return `
+      <div class="col-12 col-lg-4">  
+        <div class="card mb-5 bg-black">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${img}" class="img-fluid rounded-start" alt="${name}" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body ">
+                        <h2 class="card-title text-light text-uppercase fs-5">${name}</h2>
+                        <p class="card-text text-light fs-5">${role}</p>
+                        <a class="card-text text-decoration-none fs-54">${email}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+  `;
+};
+
+const renderTeam = () => {
+  let items = "";
+
+  for(let i = 0; i < teamMembers.length; i++) {
+    const card = createCard(teamMembers[i]);
+    items += card;
+  }
+
+  teamContainerElem.innerHTML = items;
+};
+
+// OUTPUT
+renderTeam();
